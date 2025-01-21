6b7a84147b15d10d920c4fabf708d9efbb4cbd1f
agtz92 <alfredo.gtz92@gmail.com>
2025-01-21 12:03:06 -0600

    Loading Backdrop

--- src/app/components/misc/LoadingBackdrop.js
+++ src/app/components/misc/LoadingBackdrop.js
@@ -1,22 +1,12 @@
-import React, { useState } from "react"
+import React from "react"
 import Backdrop from "@mui/material/Backdrop"
 import CircularProgress from "@mui/material/CircularProgress"
 
 const LoadingBackdrop = () => {
-  const [open, setOpen] = useState(false)
-  const handleClose = () => {
-    setOpen(false)
-  }
-  const handleOpen = () => {
-    setOpen(true)
-  }
-
   return (
     <div>
       <Backdrop
         sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
-        open={open}
-        onClick={handleClose}
       >
         <CircularProgress color="inherit" />
       </Backdrop>
